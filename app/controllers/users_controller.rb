d11d0129b24698c5512c2664d4552b69aeebd8d7
Tony Kieling <tony.kieling@gmail.com>
2019-02-21 22:28:14 +0000

    authenticate user - signup's working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     # raise user
+    # puts user
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
@@ -17,7 +18,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password_digest, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :password_confirmation)
   end
 
 end
