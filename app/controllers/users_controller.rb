aabae9015c7baa1ce22af5357a9b4600477eab59
Tony Kieling <tony.kieling@gmail.com>
2019-02-21 23:40:12 +0000

    Enhancement: Order Details Assignment's done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    # raise user
-    # puts user
+
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
@@ -18,7 +17,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
