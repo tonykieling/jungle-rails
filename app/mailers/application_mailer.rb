4e5b569386dfb004dca1b9759e3f85d6e37d9053
Tony Kieling <tony.kieling@gmail.com>
2019-02-22 21:44:18 +0000

    file structure created

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,9 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "from@example.com"
+  layout 'mailer'
+
+  def send_order_receipt
+    
+  end
+
+end
---
+++ app/mailers/send_email.rb
@@ -0,0 +1,2 @@
+class SendEmail < ApplicationMailer
+end
